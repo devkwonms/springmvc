15d5f2aa343362f8447a0ff16b7159455549b185
devkwonms <alstj1379@nate.com>
2023-01-04 19:40:48 +0900

    RequestBodyStringController - Input, Output 스트림, Reader - requestBodyStringV2 - HttpEntity - RequestBody, ResponseBody

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -1,14 +1,19 @@
 package hello.springmvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
@@ -24,5 +29,39 @@ public class RequestBodyStringController {
         log.info("messageBody = {}", messageBody);
 
         response.getWriter().write("ok");
+    }
+
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        log.info("messageBody = {}", messageBody);
+        responseWriter.write("ok");
+    }
+
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+
+        String messageBody = httpEntity.getBody();
+        log.info("messageBody = {}", messageBody);
+
+        return new HttpEntity<>("ok");
+    }
+//    @PostMapping("/request-body-string-v4")
+//    public HttpEntity<String> requestBodyStringV3(RequestEntity<String> httpEntity) throws IOException {
+//
+//        String messageBody = httpEntity.getBody();
+//        log.info("messageBody = {}", messageBody);
+//
+//        return new ResponseEntity<>("ok", HttpStatus.CREATED);
+//    }
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String messageBody) throws IOException {
+
+        log.info("messageBody = {}", messageBody);
+
+        return "ok";
     }
 }
