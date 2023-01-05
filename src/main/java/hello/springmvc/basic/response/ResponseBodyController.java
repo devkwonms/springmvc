032f2d7ddc8c9051d6e000762f5541288cd7ba75
devkwonms <alstj1379@nate.com>
2023-01-05 23:00:04 +0900

    responseBodyV1 - 서블릿을 직접 다룰 때 처럼 HttpServletResponse 객체를 통해서 HTTP 메시지 바디에 직접 ok 응답 메시지를 전달

---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,20 @@
+package hello.springmvc.basic.response;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+}
